1b050ebc1116ec5dcf2d1b40181ee10485a23b20
LittleDustDevWIN <markraph19@gmail.com>
2024-06-18 14:39:41 -0300

    refactor: MSNMessenger

---
+++ src/Computador.java
@@ -0,0 +1,12 @@
+//Simulação do uso da classe MSNMessenger
+
+public class Computador {
+	public static void main(String[] args) {
+		//Abrindo MSN Messenger
+		MSNMessenger msn = new MSNMessenger();
+		
+		msn.enviarMensagem();
+		
+		msn.receberMensagem();
+	}
+}
